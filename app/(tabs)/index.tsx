499ccf7f4dec5aed70182d52e2e766bfc702db5b
DavidNieves777 <nieveshuertadavid@gmail.com>
2025-09-16 19:11:49 -0600

    CAMBIOS EN RAMA

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,9 @@ import { View, Text, Pressable } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>🏠ESTA ES LA PAGINA PRINCIPAL</Text>
+      <Text>🏠ESTA ES LA PAGINA PRINCIPAL ESTAMOS EN RAMA</Text>
       <Pressable>CLICKME</Pressable>
+      <Text>NECESITAMOS CONFLICTOS</Text>
     </View>
   );
 }
