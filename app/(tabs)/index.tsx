d395d5947e80867754117e5f4e5a561f1edfce75
DavidNieves777 <nieveshuertadavid@gmail.com>
2025-09-16 19:09:49 -0600

    CAMBIOS EN QA

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,9 @@ import { View, Text, Pressable } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>🏠ESTA ES LA PAGINA PRINCIPAL</Text>
-      <Pressable>CLICKME</Pressable>
+      <Text>🏠ESTA ES LA PAGINA PRINCIPAL QA</Text>
+      <Pressable>CLICKME NO?</Pressable>
+      <Pressable>ESTAMOS EN QA ME PARECE</Pressable>
     </View>
   );
 }
