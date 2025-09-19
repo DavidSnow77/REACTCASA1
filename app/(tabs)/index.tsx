909488dfff9b013c4b5447a677c143cf371d63e1
DavidNieves777 <nieveshuertadavid@gmail.com>
2025-09-19 07:29:42 -0600

    CUARTO CAMBIO

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,10 @@ import { View, Text, Pressable } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>🏠PAGINA PRINCIPAL ESTAMOS EN RAMA</Text>
-      <Pressable>CLICKME</Pressable>
-      <Text>NECESITAMOS CONFLICTOS</Text>
-      <Text>
-        ESTE CAMBIO LO HIZE DESDE MI RAMA RAMA PERO LO FUSIONE EN QA ALV
-      </Text>
+      <Text>🏠PAGINA PRINCIPAL DE QA</Text>
+      <Pressable>CLICKME NO?</Pressable>
+      <Pressable>ESTAMOS EN QA ME PARECE</Pressable>
+      <Text>ESTE CAMBIO LO HIZE DESDE MI RAMA PERO VAMOS A QA</Text>
       <Text>CMABIE EL TEXTO EN RAMA PERO LO VOY A FUSIONAR EN QA S</Text>
     </View>
   );
