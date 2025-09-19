88ebc7551ac0a0d8d954f980e824108d1f453695
DavidNieves777 <nieveshuertadavid@gmail.com>
2025-09-19 07:36:12 -0600

    CAMBIOS EN RAMA

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,10 @@ export default function HomeScreen() {
       <Pressable>CLICKME NO?</Pressable>
       <Pressable>ESTAMOS EN QA ME PARECE</Pressable>
       <Text>ESTE CAMBIO LO HIZE DESDE MI RAMA PERO VAMOS A QA</Text>
+      <Text>
+        SERA QUE ESTE CAMBIO QUE HAGA EN RAMA ME TRAERA UN CONFLICTO AL FUSIONAR
+        EN QA?
+      </Text>
       <Text>CMABIE EL TEXTO EN RAMA PERO LO VOY A FUSIONAR EN QA S</Text>
     </View>
   );
