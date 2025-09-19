c99f1e904fd9cd104c70548475e28a197636ec3a
DavidNieves777 <nieveshuertadavid@gmail.com>
2025-09-19 12:41:47 -0600

    VAMOS AVER QUE SUCEDE

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,8 @@ export default function HomeScreen() {
       <Pressable>CLICKME NO?</Pressable>
       <Pressable>ESTAMOS EN QA ME PARECE</Pressable>
       <Text>ESTE CAMBIO LO HIZE DESDE MI RAMA PERO VAMOS A QA</Text>
-      <Text>CMABIE EL TEXTO EN RAMA PERO LO VOY A FUSIONAR EN QA S</Text>
+      <Text>CMABIE EL TEXTO EN RAMA PERO LO VOY A FUSIONAR EN QA SIIUU</Text>
+      <Text>VAMOS AVER QUE SUCEDE</Text>
     </View>
   );
 }
