d90ce1eafac422e2d129c52e78914af1abc6b274
DavidNieves777 <nieveshuertadavid@gmail.com>
2025-09-19 16:13:24 -0600

    COMPONIENDO RAMA

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,9 @@ import { View, Text, Pressable } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>🏠PAGINA PRINCIPAL DE QA</Text>
-      <Pressable>CLICKME NO?</Pressable>
-      <Pressable>ESTAMOS EN QA ME PARECE</Pressable>
+      <Text>🏠PAGINA PRINCIPAL DE RAMA</Text>
+      <Pressable>CLICK</Pressable>
+      <Pressable>ESTAMOS EN RAMA</Pressable>
       <Text>ESTE CAMBIO LO HIZE DESDE MI RAMA PERO VAMOS A QA</Text>
       <Text>CMABIE EL TEXTO EN RAMA PERO LO VOY A FUSIONAR EN QA SIIUU</Text>
       <Text>VAMOS AVER QUE SUCEDE</Text>
